6ac42122ee43d6085e4de58b15e2db6e372e45e3
Omar Hosny <honsyomar022@gmail.com>
2022-08-28 08:46:17 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let num = 0;
+let str = "omar";
+let isMarried = false;
+let all = "Omar";
+all = 1; // its ok
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(0, 1));
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let num: number = 0;
+let str: string = "omar";
+let isMarried: boolean = false;
+
+let all: any = "Omar";
+all = 1; // its ok
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+console.log(add(0, 1));
